6e7b81d9ce1ff045320794311c5f4d93f782355a
Robert van den Eijk <robert@vandeneijk.net>
2020-03-16 17:11:58 +0100

    Initial commit.

---
+++ src/main/java/net/vandeneijk/learn/exercisesf5bg97/ExerciseSf5bg97Application.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.exercisesf5bg97;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSf5bg97Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseSf5bg97Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/vandeneijk/learn/exercisesf5bg97/ExerciseSf5bg97ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.exercisesf5bg97;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSf5bg97ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
